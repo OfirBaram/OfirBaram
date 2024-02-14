1522a2b46dce34d8e9bde0e1c05778974a50104e
Ofir <ofirbaram@gmail.com>
2024-02-14 12:05:25 +0200

    FixText

--- application/basicNew.spec.ts
+++ application/basicNew.spec.ts
@@ -1,22 +1,66 @@
-import { remote } from 'webdriverio';
+import { remote, RemoteOptions } from 'webdriverio';
 
 describe('Basic Test', () => {
-
     it('should pass', async () => {
-        const browser = await remote({
+        // Configuration object with browser capabilities
+        const config: RemoteOptions = {
             capabilities: {
-                browserName: 'chrome', // Specify the browser name
-            },
-        });
+                browserName: 'chrome', // or 'firefox', 'edge', etc.
+                // Other capabilities if needed
+            }
+        };
 
-        // Navigate to the specified URL
-        await browser.navigateTo('http://zzzscore.com/1to50/en/?ts=1');
+        // Connect to the WebDriver instance
+        const browser = await remote(config);
 
-        console.log('Test running');
+        try {
+            // Navigate to the specified URL
+            await browser.navigateTo('http://zzzscore.com/1to50/en/?ts=1');
+            console.log('Test running');
 
-        // Example: Capture a screenshot
-        await browser.saveScreenshot('./screenshot.png');
+            let boxes = await browser.$$('.stage #grid div');
+            await getAllBoxesValue(boxes);
+            await play(boxes);
 
+        } finally {
+            // Close the WebDriver session
+            await browser.deleteSession();
+        }
 
+        async function play(boxes: WebdriverIO.ElementArray) {
+            for (let i = 1; i <= 50; i++) {
+              await clickOnBox(boxes, i);
+            }
+          }
+          
+          async function clickOnBox(boxes: WebdriverIO.ElementArray, index: number) {
+            for (let i = 0; i < boxes.length; i++) {
+              const boxText = await boxes[i].getText();
+              if (parseInt(boxText, 10) === index) {
+                await boxes[i].click();
+                console.log(`Clicked on box ${index}`);
+                return;
+              }
+            }
+          }
+          
+          
+
+        async function getAllBoxesValue(boxes: WebdriverIO.ElementArray) {
+            const boxNumbers = [];
+            for (let i = 0; i < boxes.length; i++) {
+                const boxText = await boxes[i].getText();
+                const boxNumber = parseInt(boxText, 10);
+                boxNumbers.push(boxNumber);
+            }
+            console.log('List of box numbers:', boxNumbers);
+        }
+
+        const result = await browser.$('.level');
+        if(await result.isDisplayed()){
+        console.log('1to50, Your score:' + result.getText())
+        await browser.saveScreenshot('./screenshotResult.png');
+        }
+  
     });
 });
